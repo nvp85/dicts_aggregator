a2a2a7f6a1e14b97fb53eacc0acbed5ba374299e
nvp85 <nvp@zhukov.al>
2023-02-03 23:22:49 -0800

    refactor the search view: wrap the update of the old record into a transaction

--- base_app/views.py
+++ base_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login
 from django.urls import reverse
+from django.db import transaction   
 import base_app.search
 from base_app.models import SearchHistoryRecord
 from base_app.forms import SearchForm
@@ -37,10 +38,6 @@ def search_view(request):
         search_form = SearchForm(request.POST)
         if search_form.is_valid():
             search_record = search_form.save(commit=False)
-            try:
-                old_record = SearchHistoryRecord.objects.get(word=search_record.word, user=user)
-            except SearchHistoryRecord.DoesNotExist:
-                old_record = None
             search_record.user = user
             dicts = search_form.cleaned_data['dicts']
             for dict in dicts:
@@ -48,12 +45,14 @@ def search_view(request):
                 dict_result = dict.search(search_record.word)
                 if dict_result:
                     result.append(dict_result)
-            if old_record:
-                old_record.count = 1 + old_record.count
-                old_record.save()
-            else:
-                search_form.save()
+            with transaction.atomic():
+                old_record = SearchHistoryRecord.objects.select_for_update().filter(word=search_record.word, user=user)[:1]
+                if old_record:
+                    old_record.count = 1 + old_record.count
+                    old_record.save()
+                else:
+                    search_form.save()
     else:
         search_form = SearchForm()
-    search_records = SearchHistoryRecord.objects.filter(user=user).order_by('last_date').values('word')
+    search_records = SearchHistoryRecord.objects.filter(user=user).order_by('-last_date').values('word')[:10]
     return render(request, 'search.html', {'search_form': search_form, 'result': result, 'search_records': search_records})
